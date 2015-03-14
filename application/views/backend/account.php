26c4fb7bfbfffcf599e3baa21f34815386b5ac2a
Avinash <avinash@wohlig.com>
2015-03-14 17:34:39 +0530

    c

--- application/views/backend/account.php
+++ application/views/backend/account.php
@@ -74,7 +74,7 @@
                                             <input type="text" class="form-control txt" name="fullname" placeholder="">
                                         </div>
                                     </div>
-<<<<<<< HEAD
+
                                 </div>
                                 <div class="form-group">
                                     <label class="col-lg-3 control-label txt">Email Addresss</label>
@@ -86,19 +86,7 @@
                                     <label class="col-lg-3 control-label txt">Enter Password</label>
                                     <div class="col-lg-7">
                                         <input type="password" class="form-control txt" name="choosepassword" placeholder="">
-=======
-                                    <div class="form-group">
-                                        <label class="col-lg-3 control-label txt">Email Addresss</label>
-                                        <div class="col-lg-7">
-                                            <input type="text" class="form-control txt" name="emailaddresss" placeholder="">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="col-lg-3 control-label txt">Choose Password</label>
-                                        <div class="col-lg-7">
-                                            <input type="password" class="form-control txt" name="choosepassword" placeholder="">
-                                        </div>
->>>>>>> origin/master
+
                                     </div>
                                     <div class="form-group">
                                         <label class="col-lg-3 control-label txt">Re Enter Password</label>
