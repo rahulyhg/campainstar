443ab4323f80ddb76d07708034ccd2a9f4501b00
Chintan Shah <chintan@wohlig.com>
2015-03-14 15:21:31 +0530

    change

--- application/views/login.php
+++ application/views/login.php
@@ -79,11 +79,10 @@
 		
 		<!-- HEADER -->
 		<!--===================================================-->
-		<div class="cls-header cls-header-lg">
+		<div class="cls-header cls-header-lg"  style="background:white;">
 			<div class="cls-brand">
 				<a class="box-inline" href="index.html">
-					<!-- <img alt="Nifty Admin" src="img/logo.png" class="brand-icon"> -->
-					<span class="brand-title">Campaign Stars <span class="text-thin"></span></span>
+					<img src="<?php echo base_url("assets/camp_logo.png");?>" alt="">
 				</a>
 			</div>
 		</div>
@@ -109,51 +108,19 @@
 								<input type="password" class="form-control" name="password" placeholder="Password">
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-xs-8 text-left checkbox">
-								<label class="form-checkbox form-icon">
-								<input type="checkbox"> Remember me
-								</label>
-							</div>
-							<div class="col-xs-4">
+						
 								<div class="form-group text-right">
 								<button class="btn btn-success text-uppercase" type="submit">Sign In</button>
 								</div>
-							</div>
-						</div>
-						<div class="mar-btm"><em>- or -</em></div>
-						<button class="btn btn-primary btn-lg btn-block" type="button">
-							<i class="fa fa-facebook fa-fw"></i> Login with Facebook
-						</button>
+							
 					</form>
 				</div>
 			</div>
-			<div class="pad-ver">
-				<a href="pages-password-reminder.html" class="btn-link mar-rgt">Forgot password ?</a>
-				<a href="pages-register.html" class="btn-link mar-lft">Create a new account</a>
-			</div>
+		
 		</div>
 		<!--===================================================-->
 		
-		
-		<!-- DEMO PURPOSE ONLY -->
-		<!--===================================================-->
-		<div class="demo-bg">
-			<div id="demo-bg-list">
-				<div class="demo-loading"><i class="fa fa-refresh"></i></div>
-				<img class="demo-chg-bg bg-trans" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-trns.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-1.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg active" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-2.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-3.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-4.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-5.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-6.jpg'); ?>" alt="Background Image">
-				<img class="demo-chg-bg" src="<?php echo base_url('assets/img/bg-img/thumbs/bg-img-7.jpg'); ?>" alt="Background Image">
-			</div>
-		</div>
-		<!--===================================================-->
-		
-		
+	
 		
 	</div>
 	<!--===================================================-->
